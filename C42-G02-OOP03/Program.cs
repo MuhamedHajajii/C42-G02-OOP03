34567bb72a500ee0d4def4bd1eda8de39d1e5dce
haji <muhamedhajajii@outlook.com>
2024-08-03 09:10:47 +0300

    feat(@Progrm): Add OverLoading

--- C42-G02-OOP03/Program.cs
+++ C42-G02-OOP03/Program.cs
@@ -1,7 +1,37 @@
-﻿namespace C42_G02_OOP03
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace C42_G02_OOP03
 {
     internal class Program
     {
+
+        #region OverLoading
+
+        public static int Sum(int x, int y)
+        {
+            return x * y;
+        }
+
+        // Change The Number Of Parameters
+        public static int Sum(int x, int y, int z)
+        {
+            return x * y * z;
+        }
+
+        // Change The Type of Parameters
+        public static string Sum(string Name, int y, int z)
+        {
+            return $"Name = {Name} , Number = { y * z}";
+        }
+
+        // Change The Order Of the datatype of the parameters
+        public static string Sum( int y, string Name, int z)
+        {
+            return $"Name = {Name} , Number = {y * z}";
+        }
+
+        #endregion
+
         static void Main()
         {
             // RelationShip Between Classes
@@ -19,6 +49,45 @@
              */
 
             #endregion
+
+            #region 2- PolyMorphism - OverLoading
+
+            /** What is the meaning of polymorphism
+             * poly = Many
+             * morphism = forms
+             * so its mean many types of something
+             */
+
+            /** Types Of OverLoading
+             * 1- Functions(Methods) overloading
+             * 2- Constructor OverLoading 
+             * 3- Indexer Overloading
+             * 
+             * You can make every one of them called by three things [Type, Numbers, order] of parameters
+             * 
+             * If You Change The Parameters and don't change the logic this is still repeatation
+             * 
+             * Load More on the same function
+             * 
+             * The OverLoading with the Methods is just for readablility
+             * like the console.WriteLine(); You already has 18 types from it
+             * 
+             */
+
+            /** Overriding
+             * When You inherit methods from another calss and want to change the behavior of it
+             */
+
+            //Console.WriteLine(Sum(1,4));
+            //Console.WriteLine(Sum(1,2,3));
+            //Console.WriteLine(Sum("Muhamed Hajajii",5,4));
+            //Console.WriteLine(Sum(1, "Muhamed Hajajii", 4));
+
+
+            #endregion
+
+
+
         }
     }
 }
