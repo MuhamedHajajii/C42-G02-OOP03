27af939f673c7f96d276ea7cef7b1de6675da179
haji <muhamedhajajii@outlook.com>
2024-08-03 08:58:49 +0300

    feat(@Program): Add First Video The Relation Ship Between Classes

--- C42-G02-OOP03/Program.cs
+++ C42-G02-OOP03/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main()
         {
-            
+            // RelationShip Between Classes
+            // PolyMorphism [OverLoading]
+            // Polymorphism [OverRiding]
+            //
+
+            #region 1- Association - Inheritance
+
+            /** Not All The Realtion Between Classes is Inheritance
+             * The Full Time Employee is a Employee [Inheritance] 
+             * The Room Has Chairs [ Association - Aggregation ] if there are no charis still there a room [Partial]
+             * The Room Has Walls [ Association - Composition ] if there are no walls so no Room [Mendatory]
+             * The Order Has An item [ Association - Composition ] if there are no item no order [Mendatory]
+             */
+
+            #endregion
         }
     }
 }
